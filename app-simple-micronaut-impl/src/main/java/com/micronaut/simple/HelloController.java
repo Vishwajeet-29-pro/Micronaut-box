8937e6be9afe7ddaca04ed9c2d9a247c1d2c29a3
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-01 15:26:42 +0530

    feat: Add method which takes query and make test pass

--- app-simple-micronaut-impl/src/main/java/com/micronaut/simple/HelloController.java
+++ app-simple-micronaut-impl/src/main/java/com/micronaut/simple/HelloController.java
@@ -2,6 +2,7 @@ package com.micronaut.simple;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
 
 @Controller("/hello")
 public class HelloController {
@@ -9,5 +10,10 @@ public class HelloController {
     @Get
     public String index() {
         return "Hello Micronaut";
+    }
+
+    @Get("/query")
+    public String queryMethod(@QueryValue(defaultValue = "Micronaut") String value) {
+        return "Hello "+value;
     }
 }
