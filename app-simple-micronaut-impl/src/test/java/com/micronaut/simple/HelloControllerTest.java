53e911af51852c728cdf39fdca0b61ffbef79ecf
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-01 15:14:16 +0530

    test: Add test for hello controller

---
+++ app-simple-micronaut-impl/src/test/java/com/micronaut/simple/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.micronaut.simple;
+
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest
+class HelloControllerTest {
+
+    @Inject
+    @Client("/")
+    HttpClient httpClient;
+
+    @Test
+    void testHelloController() {
+        String response = httpClient.toBlocking().retrieve("/hello");
+
+        assertEquals("Hello Micronaut", response);
+    }
+}
