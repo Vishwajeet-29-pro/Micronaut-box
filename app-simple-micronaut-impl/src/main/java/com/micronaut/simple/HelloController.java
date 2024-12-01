0b2d8520a8148f3e9659d864b7aee0cf58eba3d1
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-01 15:45:15 +0530

    feat: Add method for path variable and make test pass

--- app-simple-micronaut-impl/src/main/java/com/micronaut/simple/HelloController.java
+++ app-simple-micronaut-impl/src/main/java/com/micronaut/simple/HelloController.java
@@ -2,6 +2,7 @@ package com.micronaut.simple;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.QueryValue;
 
 @Controller("/hello")
@@ -15,5 +16,16 @@ public class HelloController {
     @Get("/query")
     public String queryMethod(@QueryValue(defaultValue = "Micronaut") String value) {
         return "Hello "+value;
+    }
+
+    /**
+     *    New Thing to learn no need of @PathVariable annotation
+     *    simply pass the same variable as defined in path
+     *    here `name` from @Get and `name` from method parameter is same
+     *    You can also add @PathVariable annotation same as spring
+    */
+    @Get("/{name}")
+    public String pathVariableMethod(String name) {
+        return "Hello "+name;
     }
 }
