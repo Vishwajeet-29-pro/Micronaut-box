aa6f81aabaadb74ad63c03e134f9f5b0faf50a67
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-04 22:12:06 +0530

    refactor: Rename Main class

---
+++ app-h2-db-connection/src/main/java/com/micronaut/h2/H2Application.java
@@ -0,0 +1,9 @@
+package com.micronaut.h2;
+
+import io.micronaut.runtime.Micronaut;
+
+public class H2Application {
+    public static void main(String[] args) {
+        Micronaut.run(H2Application.class);
+    }
+}
--- app-h2-db-connection/src/main/java/com/micronaut/h2/Main.java
+++
@@ -1,7 +0,0 @@
-package com.micronaut.h2;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
