a19b8a46162e8a71d98df44f016fff9449aaa26c
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-04 22:09:09 +0530

    feat: Add setup for h2 application with micronaut

---
+++ app-h2-db-connection/src/main/java/com/micronaut/h2/Main.java
@@ -0,0 +1,7 @@
+package com.micronaut.h2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "Micronaut-box"
 include("app-simple-micronaut-impl")
+include("app-h2-db-connection")
