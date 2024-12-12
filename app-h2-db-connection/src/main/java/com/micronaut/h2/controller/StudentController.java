9343c80a7863fd71eea9a84c63f457e4c3aa85a7
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-12 22:23:37 +0530

    feat: Add code for POST mapping

--- app-h2-db-connection/src/main/java/com/micronaut/h2/controller/StudentController.java
+++ app-h2-db-connection/src/main/java/com/micronaut/h2/controller/StudentController.java
@@ -1,7 +1,12 @@
 package com.micronaut.h2.controller;
 
+import com.micronaut.h2.dto.StudentRequest;
+import com.micronaut.h2.dto.StudentResponse;
 import com.micronaut.h2.service.StudentService;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
 import lombok.RequiredArgsConstructor;
 
 @Controller("/api/v1/students")
@@ -10,4 +15,9 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Post
+    public HttpResponse<StudentResponse> createStudent(@Body StudentRequest studentRequest) {
+        StudentResponse response = studentService.createStudent(studentRequest);
+        return HttpResponse.created(response);
+    }
 }
