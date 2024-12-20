f15a6c74dfe0f17dfd89dc2ede25d2789d4818eb
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-20 21:35:02 +0530

    feat: Add annotations on Main class to enable OpenApi

--- app-h2-db-connection/src/main/java/com/micronaut/h2/H2Application.java
+++ app-h2-db-connection/src/main/java/com/micronaut/h2/H2Application.java
@@ -1,7 +1,16 @@
 package com.micronaut.h2;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Micronaut-h2-database-connection",
+                version = "1.0"
+        ), servers = @Server(url = "http://localhost:8080/")
+)
 public class H2Application {
     public static void main(String[] args) {
         Micronaut.run(H2Application.class);
