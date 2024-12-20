288769b6b71f87b4442e2eabfb0e46e5a5096364
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-20 08:03:48 +0530

    feat: Add GET controller for GET all students and make test pass

--- app-h2-db-connection/src/main/java/com/micronaut/h2/controller/StudentController.java
+++ app-h2-db-connection/src/main/java/com/micronaut/h2/controller/StudentController.java
@@ -6,8 +6,11 @@ import com.micronaut.h2.service.StudentService;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @Controller("/api/v1/students")
 @RequiredArgsConstructor
@@ -19,5 +22,11 @@ public class StudentController {
     public HttpResponse<StudentResponse> createStudent(@Body StudentRequest studentRequest) {
         StudentResponse response = studentService.createStudent(studentRequest);
         return HttpResponse.created(response);
+    }
+
+    @Get
+    public HttpResponse<List<StudentResponse>> getStudentsByList() {
+        List<StudentResponse> studentResponses = studentService.findAllStudents();
+        return HttpResponse.ok(studentResponses);
     }
 }
