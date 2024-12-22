d2ed32a8c0dd2022d69aba917b844359d161f154
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-22 15:48:41 +0530

    refactor: Start using to jakarta persistence package instead of micronaut data

--- app-h2-db-connection/src/main/java/com/micronaut/h2/model/Student.java
+++ app-h2-db-connection/src/main/java/com/micronaut/h2/model/Student.java
@@ -1,20 +1,21 @@
 package com.micronaut.h2.model;
 
-import io.micronaut.data.annotation.GeneratedValue;
-import io.micronaut.data.annotation.Id;
-import io.micronaut.data.annotation.MappedEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@MappedEntity
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Student {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String studentName;
     private int age;
