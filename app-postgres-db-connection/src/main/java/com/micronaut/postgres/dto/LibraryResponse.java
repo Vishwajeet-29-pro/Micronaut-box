68bed41b1812bddb9fa10aa9b1156796a6d4794b
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-25 08:07:49 +0530

    feat: Add response dto class

---
+++ app-postgres-db-connection/src/main/java/com/micronaut/postgres/dto/LibraryResponse.java
@@ -0,0 +1,27 @@
+package com.micronaut.postgres.dto;
+
+import com.micronaut.postgres.model.Library;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+public class LibraryResponse {
+
+    private UUID id;
+    private String name;
+    private String location;
+    private LocalDate establishDate;
+    private boolean isActive;
+
+    public static LibraryResponse toResponse(Library library) {
+        return new LibraryResponse(
+                library.getId(), library.getName(),
+                library.getLocation(), library.getDate(),
+                library.isActive()
+        );
+    }
+}
