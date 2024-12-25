f137bfa79211e24d184fdd4d55cea0574b07c38c
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-25 07:57:09 +0530

    feat: Add entity class Library

---
+++ app-postgres-db-connection/src/main/java/com/micronaut/postgres/model/Library.java
@@ -0,0 +1,27 @@
+package com.micronaut.postgres.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Library {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String location;
+    private LocalDate date;
+    private boolean isActive;
+}
