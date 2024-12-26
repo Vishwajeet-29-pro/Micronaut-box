2fc5a3352907f06bf09b4fe32d7512dc867a1675
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2024-12-26 08:19:24 +0530

    test: Add test for add library details

--- app-postgres-db-connection/src/test/java/com/micronaut/postgres/service/LibraryServiceTest.java
+++ app-postgres-db-connection/src/test/java/com/micronaut/postgres/service/LibraryServiceTest.java
@@ -2,17 +2,22 @@ package com.micronaut.postgres.service;
 
 import com.micronaut.postgres.dto.LibraryRequest;
 import com.micronaut.postgres.dto.LibraryResponse;
+import com.micronaut.postgres.model.Library;
 import com.micronaut.postgres.repository.LibraryRepository;
 import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @MicronautTest
 class LibraryServiceTest {
@@ -28,11 +33,23 @@ class LibraryServiceTest {
     }
 
     private LibraryRequest libraryRequest;
-    private LibraryResponse libraryResponse;
+    private Library library;
 
     @BeforeEach
     public void setup() {
         libraryRequest = new LibraryRequest("Central Library", "SB road, Pune", LocalDate.of(2004, 1, 1), true);
-        libraryResponse = new LibraryResponse(UUID.randomUUID(), "Central Library", "SB road, Pune", LocalDate.of(2004, 1, 1), true);
+        library = new Library(UUID.randomUUID(), "Central Library", "SB road, Pune", LocalDate.of(2004, 1, 1), true);
+    }
+
+    @Test
+    void test_create_book_should_return_book_response() {
+        when(libraryRepository.save(any(Library.class))).thenReturn(library);
+
+        LibraryResponse libraryResponse = libraryService.addLibraryDetails(libraryRequest);
+
+        assertNotNull(libraryResponse);
+        assertEquals("Central Library", libraryResponse.getName());
+        assertEquals("SB road, Pune", libraryResponse.getLocation());
+        assertEquals(LocalDate.of(2004, 1, 1), libraryResponse.getEstablishDate());
     }
 }
