dc9b1baf5f5415ad696857cfefcf49c849ef3a62
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-02 08:03:36 +0530

    feat: Add POST controller mapping

--- app-postgres-db-connection/src/main/java/com/micronaut/postgres/LibraryController.java
+++ app-postgres-db-connection/src/main/java/com/micronaut/postgres/LibraryController.java
@@ -1,11 +1,21 @@
 package com.micronaut.postgres;
 
+import com.micronaut.postgres.dto.LibraryRequest;
+import com.micronaut.postgres.dto.LibraryResponse;
 import com.micronaut.postgres.service.LibraryService;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
 import lombok.RequiredArgsConstructor;
 
 @Controller("/api/v1/library")
 @RequiredArgsConstructor
 public class LibraryController {
     private final LibraryService libraryService;
+
+    @Post
+    public HttpResponse<LibraryResponse> createLibrary(@Body LibraryRequest libraryRequest) {
+        return HttpResponse.created(libraryService.addLibraryDetails(libraryRequest));
+    }
 }
