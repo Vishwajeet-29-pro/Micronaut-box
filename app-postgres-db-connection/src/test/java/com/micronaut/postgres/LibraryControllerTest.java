054f87b9264579d7fe3241649fce45d0c36036f9
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-02 07:53:30 +0530

    test: Add test for add library details

--- app-postgres-db-connection/src/test/java/com/micronaut/postgres/LibraryControllerTest.java
+++ app-postgres-db-connection/src/test/java/com/micronaut/postgres/LibraryControllerTest.java
@@ -1,14 +1,23 @@
 package com.micronaut.postgres;
 
+import com.micronaut.postgres.dto.LibraryRequest;
+import com.micronaut.postgres.dto.LibraryResponse;
 import com.micronaut.postgres.service.LibraryService;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @MicronautTest
 class LibraryControllerTest {
@@ -23,5 +32,23 @@ class LibraryControllerTest {
     @MockBean(LibraryService.class)
     LibraryService mockLibraryService() {
         return mock(LibraryService.class);
+    }
+
+    @Test
+    void test_post_library_should_return_created() {
+        LibraryRequest libraryRequest = new LibraryRequest("Central Library", "SB road, Pune", LocalDate.of(2004, 1, 1), true);
+        LibraryResponse libraryResponse = new LibraryResponse(UUID.randomUUID(), "Central Library", "SB road, Pune", LocalDate.of(2004, 1, 1), true);
+
+        when(libraryService.addLibraryDetails(libraryRequest)).thenReturn(libraryResponse);
+
+        HttpResponse<LibraryResponse> libraryResponseHttpResponse = httpClient.toBlocking().exchange(
+                HttpRequest.POST("/api/v1/students", libraryRequest),
+                LibraryResponse.class
+        );
+
+        assertEquals(201, libraryResponseHttpResponse.getStatus().getCode());
+        assertNotNull(libraryResponseHttpResponse);
+        assertEquals("Central Library", libraryResponseHttpResponse.body().getName());
+        assertTrue(libraryResponseHttpResponse.body().isActive());
     }
 }
