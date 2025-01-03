9b3f0c09e6454f5f1b62d9e4b631cabccb9c4de1
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-03 21:19:13 +0530

    feat: Implement GET controller for retrieve all libraries

--- app-postgres-db-connection/src/main/java/com/micronaut/postgres/controller/LibraryController.java
+++ app-postgres-db-connection/src/main/java/com/micronaut/postgres/controller/LibraryController.java
@@ -6,8 +6,11 @@ import com.micronaut.postgres.service.LibraryService;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @Controller("/api/v1/library")
 @RequiredArgsConstructor
@@ -17,5 +20,10 @@ public class LibraryController {
     @Post
     public HttpResponse<LibraryResponse> createLibrary(@Body LibraryRequest libraryRequest) {
         return HttpResponse.created(libraryService.addLibraryDetails(libraryRequest));
+    }
+
+    @Get
+    public HttpResponse<List<LibraryResponse>> retrieveAllLibraries() {
+        return HttpResponse.ok(libraryService.findAllLibraries());
     }
 }
