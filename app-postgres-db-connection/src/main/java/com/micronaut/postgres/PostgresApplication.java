5cd291db7a7138967ee2fe857c7c82ac325dfaa2
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-04 08:27:59 +0530

    fix: Big Mistake, forgot to configure micronaut application

--- app-postgres-db-connection/src/main/java/com/micronaut/postgres/PostgresApplication.java
+++ app-postgres-db-connection/src/main/java/com/micronaut/postgres/PostgresApplication.java
@@ -1,7 +1,9 @@
 package com.micronaut.postgres;
 
+import io.micronaut.runtime.Micronaut;
+
 public class PostgresApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Micronaut.run(PostgresApplication.class);
     }
 }
