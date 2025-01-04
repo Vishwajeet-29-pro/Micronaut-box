d437663ffb238418235615c0607c7bc4bfab84a7
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-04 08:33:12 +0530

    feat: Configure OpenApi documentation with postgres application

--- app-postgres-db-connection/src/main/java/com/micronaut/postgres/PostgresApplication.java
+++ app-postgres-db-connection/src/main/java/com/micronaut/postgres/PostgresApplication.java
@@ -1,7 +1,16 @@
 package com.micronaut.postgres;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Micronaut-postgres-database-connection",
+                version = "1.0"
+        ), servers = @Server(url = "http://localhost:8080/")
+)
 public class PostgresApplication {
     public static void main(String[] args) {
         Micronaut.run(PostgresApplication.class);
