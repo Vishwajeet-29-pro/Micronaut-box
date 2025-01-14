f4fa6b855700af674d058c63c815f1ee349f6319
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-14 21:25:45 +0530

    feat: Implement method for add new product

--- app-mysql-db-connection/src/main/java/com/micronaut/mysql/controller/ProductController.java
+++ app-mysql-db-connection/src/main/java/com/micronaut/mysql/controller/ProductController.java
@@ -1,7 +1,12 @@
 package com.micronaut.mysql.controller;
 
+import com.micronaut.mysql.dto.ProductRequest;
+import com.micronaut.mysql.dto.ProductResponse;
 import com.micronaut.mysql.service.ProductService;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
 import lombok.RequiredArgsConstructor;
 
 @Controller("/api/v1/products")
@@ -9,4 +14,9 @@ import lombok.RequiredArgsConstructor;
 public class ProductController {
 
     private final ProductService productService;
+
+    @Post
+    public HttpResponse<ProductResponse> addNewProduct(@Body ProductRequest productRequest) {
+        return HttpResponse.created(productService.addProduct(productRequest));
+    }
 }
