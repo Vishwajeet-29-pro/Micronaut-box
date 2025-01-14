8930301c9c7374d3b7bad852105edf5e99e90c94
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-14 21:44:12 +0530

    test: Add test for fetch all products

--- app-mysql-db-connection/src/test/java/com/micronaut/mysql/controller/ProductControllerTest.java
+++ app-mysql-db-connection/src/test/java/com/micronaut/mysql/controller/ProductControllerTest.java
@@ -3,6 +3,7 @@ package com.micronaut.mysql.controller;
 import com.micronaut.mysql.dto.ProductRequest;
 import com.micronaut.mysql.dto.ProductResponse;
 import com.micronaut.mysql.service.ProductService;
+import io.micronaut.core.type.Argument;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.HttpClient;
@@ -13,6 +14,7 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -36,7 +38,7 @@ class ProductControllerTest {
     @Test
     void test_create_product_should_return_product_response_with_201_created() {
         ProductRequest productRequest = new ProductRequest("Mobile", "Oppo Reno 14", 50000.0, 50, LocalDateTime.of(2025, 1, 14, 8, 0));
-        ProductResponse productResponse = new ProductResponse(1L,"Mobile", "Oppo Reno 14", 50000.0, 50, LocalDateTime.of(2025, 1, 14, 8, 0));
+        ProductResponse productResponse = new ProductResponse(1L, "Mobile", "Oppo Reno 14", 50000.0, 50, LocalDateTime.of(2025, 1, 14, 8, 0));
 
         when(productService.addProduct(productRequest)).thenReturn(productResponse);
 
@@ -54,7 +56,7 @@ class ProductControllerTest {
 
     @Test
     void test_get_product_by_id_should_return_product_response_with_id_and_200_ok() {
-        ProductResponse productResponse = new ProductResponse(1L,"Mobile", "Oppo Reno 14", 50000.0, 50, LocalDateTime.of(2025, 1, 14, 8, 0));
+        ProductResponse productResponse = new ProductResponse(1L, "Mobile", "Oppo Reno 14", 50000.0, 50, LocalDateTime.of(2025, 1, 14, 8, 0));
 
         when(productService.findProductById(1L)).thenReturn(productResponse);
 
@@ -68,5 +70,26 @@ class ProductControllerTest {
         assertEquals("Mobile", productResponseHttpResponse.body().getName());
         assertEquals(50000.0, productResponseHttpResponse.body().getPrice());
         assertEquals(productResponse.getAddedAt(), productResponseHttpResponse.body().getAddedAt());
+    }
+
+    @Test
+    void test_fetch_all_products_should_return_list_of_product_response_and_200_ok() {
+        List<ProductResponse> productResponses = List.of(
+                new ProductResponse(1L, "Mobile", "Oppo Reno 14", 50000.0, 50, LocalDateTime.of(2025, 1, 14, 8, 0)),
+                new ProductResponse(2L, "Laptop", "Dell something", 65000.0, 40, LocalDateTime.of(2025, 1, 14, 8, 0))
+        );
+
+        when(productService.findAllProducts()).thenReturn(productResponses);
+
+        HttpResponse<List<ProductResponse>> listHttpResponse = httpClient.toBlocking().exchange(
+                HttpRequest.GET("/api/v1/products"),
+                Argument.listOf(ProductResponse.class)
+        );
+
+        assertNotNull(listHttpResponse);
+        assertEquals(2, listHttpResponse.body().size());
+        assertEquals("Laptop", listHttpResponse.body().getFirst().getName());
+        assertEquals(65000.0, listHttpResponse.body().getLast().getPrice());
+        assertEquals(50, listHttpResponse.body().getFirst().getStockQuantity());
     }
 }
