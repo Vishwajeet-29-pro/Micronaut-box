ecbe04484f91dab006e716ea473b9b4bb2539d74
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-14 21:35:21 +0530

    feat: Add GET controller endpoint to fetch product by id and make test pass

--- app-mysql-db-connection/src/main/java/com/micronaut/mysql/controller/ProductController.java
+++ app-mysql-db-connection/src/main/java/com/micronaut/mysql/controller/ProductController.java
@@ -4,9 +4,7 @@ import com.micronaut.mysql.dto.ProductRequest;
 import com.micronaut.mysql.dto.ProductResponse;
 import com.micronaut.mysql.service.ProductService;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.annotation.Body;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.*;
 import lombok.RequiredArgsConstructor;
 
 @Controller("/api/v1/products")
@@ -18,5 +16,10 @@ public class ProductController {
     @Post
     public HttpResponse<ProductResponse> addNewProduct(@Body ProductRequest productRequest) {
         return HttpResponse.created(productService.addProduct(productRequest));
+    }
+
+    @Get("/{id}")
+    public HttpResponse<ProductResponse> findProductById(@PathVariable Long id) {
+        return HttpResponse.ok(productService.findProductById(id));
     }
 }
