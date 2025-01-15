9db52b3a5f7dcc5ab4334585fe5ee6bb11ef8964
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-15 21:58:43 +0530

    feat: Configure main class for handling swagger stuff

--- app-mysql-db-connection/src/main/java/com/micronaut/mysql/MySQLApplication.java
+++ app-mysql-db-connection/src/main/java/com/micronaut/mysql/MySQLApplication.java
@@ -1,7 +1,16 @@
 package com.micronaut.mysql;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Micronaut-MySQL-database-connection",
+                version = "1.0"
+        ), servers = @Server(url = "http://localhost:8080/")
+)
 public class MySQLApplication {
     public static void main(String[] args) {
         Micronaut.run(MySQLApplication.class);
