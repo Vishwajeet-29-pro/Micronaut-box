0c29e0707899a4e578372354c4993af7d8642387
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-17 07:47:07 +0530

    feat: Add new module for implementation of Micronaut using Kotlin

---
+++ app-kotlin-implementation/src/main/kotlin/com/micronaut/kotlin_impl/Application.kt
@@ -0,0 +1,9 @@
+package com.micronaut.kotlin_impl
+
+import io.micronaut.runtime.Micronaut.run
+
+object Application {}
+
+fun main(args: Array<String>) {
+    run(*args)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,10 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
 rootProject.name = "Micronaut-box"
 include("app-simple-micronaut-impl")
 include("app-h2-db-connection")
 include("app-postgres-db-connection")
 include("app-mysql-db-connection")
 include("common-config")
+include("app-kotlin-implementation")
