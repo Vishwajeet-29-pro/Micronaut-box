d7ae56d8728e7a50b0672663207a48956aa831ad
Vishwajeet-29-pro <vishwajeetak@gmail.com>
2025-01-21 07:57:50 +0530

    build: Move plugins to root build.gradle.kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.25" apply false
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.9.25" apply false
+    id("com.google.devtools.ksp") version "1.9.25-1.0.20" apply false
 }
 
 repositories {
